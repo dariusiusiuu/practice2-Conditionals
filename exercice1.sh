aea9f4800b72b79965a7ca8833f96a903da93e5d
dariusiusiuu <dsanchezr@ies-sabadell.cat>
2025-05-02 12:10:35 +0200

    Added

--- exercice1.sh
+++ exercice1.sh
@@ -2,10 +2,8 @@
 
 read -p "Enter an integer: " number
 
-if [[ $number -gt 0 ]]; then
+if ((number >= 0)); then
     echo "The number $number is positive"
-elif [[ $number -lt 0 ]]; then
-    echo "The number $number is negative"
 else
-    echo "The number is zero"
+    echo "The number $number is negative"
 fi
